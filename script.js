14e4cb261544d65daa536fe07cae24a3d7dbe42a
Bashir Rashid <karche475b@gmail.com>
2023-08-02 09:31:52 +0300

    Solution of GRADE activity

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("hello");
+
+
+let grade = 82
+
+if(grade <=100 && grade >= 90)
+{
+     console.log(`you got an A`)
+}
+else if(grade <=89 && grade >= 75)
+{
+     console.log(`you got a B`)
+}
+else if(grade <=74 && grade >= 60)
+{
+     console.log(`you got a C`)
+}
+else if(grade < 60)
+{
+     console.log(`you got an F_ailed`)
+}
+else{
+     console.log(`error`)
+}
